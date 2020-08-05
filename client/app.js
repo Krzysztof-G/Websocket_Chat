84dbac845a142f3ff8d5c8507791cbfea89b44ca
Krzysztof Gumuła <k.gumula@icloud.com>
2020-08-05 17:37:32 +0200

    add files index style and app

---
+++ client/app.js
@@ -0,0 +1,10 @@
+// Reference
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-section__list');
+const addMessageForm = document.getElementById('messages-section__list');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+
+// Global variables
+let userName; 
